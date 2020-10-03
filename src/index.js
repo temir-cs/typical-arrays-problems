78fa8d7cbfc4c6ae23167e9677a39d781b0971b6
temir-cs <temirlan.akhanov@yandex.ru>
2020-10-03 13:45:09 +0600

    feat: impelemented base functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  // Traditional way
+  /* let min = array[0];
+  for (const num of array) {
+    if (num < min) {
+      min = num;
+    }
+  }
+  return min; */
+
+  // Functional way
+  return array.reduce((min, current) => current < min ? current : min, array[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  // Traditional way
+  /* let max = array[0];
+  for (const num of array) {
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max; */
+
+  // Functional way
+  return array.reduce((max, current) => current > max ? current : max, array[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length === 0) {
+    return 0;
+  }
+  // Traditional way
+  /* let total = 0;
+  for (const num of array) {
+    total += num;
+  }
+  const avg = total / array.length;
+  return avg; */
+  
+  // Functional way
+  return array.reduce((total, current) => total += current, 0) / array.length;
 }
